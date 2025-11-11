fa1af59c6ec8e1ec680d5ca76148feba9d0b034c
VitorGuilherme <alfredo.neg9@gmail.com>
2025-11-11 14:03:03 -0300

    :sparkles: added navbar css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
+//components
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+//pages
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Home from "./pages/Home/Home";
+
+//router
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
-  return <h1>ReactGram</h1>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import "./Navbar.css";
+
+//components
+import { BsHouseDoorFill, BsSearch } from "react-icons/bs";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="nav">
+      <Link to="/">ReactGram</Link>
+      <form id="search-form">
+        <BsSearch />
+        <input type="text" placeholder="Pesquisar" />
+      </form>
+      <ul id="nav-links">
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastre-se</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
